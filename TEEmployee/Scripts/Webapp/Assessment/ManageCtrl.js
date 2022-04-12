7695d9758266aa2007b5af92cf4b42520b30fc58
Donald <lugenius2001@gmail.com>
2022-04-12 11:50:47 +0800

    更新主管評分表主題

--- TEEmployee/Scripts/Webapp/Assessment/ManageCtrl.js
+++ TEEmployee/Scripts/Webapp/Assessment/ManageCtrl.js
@@ -9,17 +9,33 @@ app.service('appService', ['$http', function ($http) {
     this.GetManageAssessments = function (o) {
         return $http.post('Assessment/GetManageAssessments', o);
     };
+
+    this.CreateResponse = function (o) {
+        return $http.post('Assessment/CreateResponse', o);
+    };
 }]);
 
 app.controller('ManageCtrl', ['$scope', '$window', 'appService', '$rootScope', function ($scope, $window, appService, $rootScope) {
 
-    $scope.ManageAssessments = [];
-    appService.GetManageAssessments({})
+    $scope.ManageAssessments = {};
+
+    $scope.response = {
+        "Id": 7291,
+        "choices": []
+    }
+
+    $scope.CreateResponse = function () {
+        appService.CreateResponse($scope.response)
+            .then(function (ret) {
+                $window.location.href = '/Home';
+            });
+    }
+
+    appService.GetManageAssessments({ id: $scope.$id })
         .then(function (ret) {
             $scope.ManageAssessments = ret.data;
         })
         .catch(function (ret) {
             alert('Error');
         });
-
 }]);
