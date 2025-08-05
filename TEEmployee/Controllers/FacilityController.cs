cd97cfdde1de34308833e9bb8cdb06177c662691
Vincent <vinsino7596@gmail.com>
2025-08-05 13:10:41 +0800

    Add Facility controller

---
+++ TEEmployee/Controllers/FacilityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEEmployee.Controllers
+{
+    public class FacilityController : Controller
+    {
+        // GET: Facility
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
