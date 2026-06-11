27a82bb7360a2742763a7c07068d33a237aaa838
SinoDonald <donaldlu@mail.sinotech.com.tw>
2026-06-11 09:02:35 +0800

    建立WishController

---
+++ TEEmployee/Controllers/WishController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEEmployee.Controllers
+{
+    public class WishController : Controller
+    {
+        // GET: Wish
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
